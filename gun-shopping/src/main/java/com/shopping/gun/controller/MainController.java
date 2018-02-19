f3b8e2c937f2db8b607f31a5245bdf41ff28d682
emori_sota <emori_sota@cyberagent.co.jp>
2018-02-19 17:41:32 +0900

    add js css file

--- gun-shopping/src/main/java/com/shopping/gun/controller/MainController.java
+++ gun-shopping/src/main/java/com/shopping/gun/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.shopping.gun.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shopping.gun.model.User;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	User user;
+	
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		
+		user.setName("emo");
+		
+		model.addAttribute("name", user.getName());
 		
 		//String redirectPath = "/index";
 		return "index";
--- gun-shopping/src/main/java/com/shopping/gun/model/User.java
+++ gun-shopping/src/main/java/com/shopping/gun/model/User.java
@@ -1,7 +1,35 @@
 package com.shopping.gun.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class User {
 	
+	@NotNull
+	@Size(min = 1, max = 12)
+	int id;
 	
+	@NotNull
+	String name;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
